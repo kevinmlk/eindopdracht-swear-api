0006e988618c5199b1376edb8dc7c93ce74a9d6a
Kevin Malekera <r0900654@student.thomasmore.be>
2024-11-02 17:41:50 +0100

    feat(api): added logic for index route in controller

---
+++ controllers/api/v1/orders.js
@@ -0,0 +1,25 @@
+// Include model
+const Order = require('../../../models/Order');
+
+// Define controllers
+
+// Get all orders - index
+const index = async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.json({
+      "status": "success",
+      "message": "GETTING orders",
+      "data": {
+        "orders": orders
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Export controllers
+module.exports = {
+  index
+};
