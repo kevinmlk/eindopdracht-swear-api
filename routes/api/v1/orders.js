4208e29fb3df87d7dd6e1cb5fc7825e5d38def04
Kevin Malekera <r0900654@student.thomasmore.be>
2024-11-02 16:50:24 +0100

    feat(api): added orders route and defined some api routes

---
+++ routes/api/v1/orders.js
@@ -0,0 +1,18 @@
+// Include express
+const express = require('express');
+
+// Include router
+const router = express.Router();
+
+// Include controller
+const OrdersController = require('../../../controllers/api/v1/orders');
+
+// Define routes
+router.get('/', OrdersController.index);
+router.get('/:id', OrdersController.show);
+router.post('/', OrdersController.create);
+router.patch('/:id', OrdersController.update); // Only for admins
+router.delete('/:id', OrdersController.destroy); // Only for admins
+
+// Export routes
+module.exports = router;
